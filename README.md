b9a2dff14f47ebae52287a689ac213185ecc2177
Alan Chuang <Alan@gmail.com>
2023-01-12 01:42:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Welcome to Alan's Chuang personal blog
+
+Here is the place to record my portfolio and some notes.
+Please feel free to contact me if you have any suggestion!
---
+++ js/main.js
@@ -0,0 +1,105 @@
+/* header內的bar點擊縮放效果 */
+(function () {
+    let faBar = document.querySelector(".fa-bar");
+    let spans = document.querySelectorAll(".fa-bar span");
+    let navBar = document.querySelector(".navbar");
+    let navAList = document.querySelectorAll(".navbar a");
+    faBar.addEventListener("click", () => {
+        spans.forEach((span) => {
+            span.classList.toggle("actived");
+            if (span.className === "actived") {
+                navBar.style.top = "0";
+            } else {
+                navBar.style.top = -120 + "%";
+            }
+        })
+    })
+    navAList.forEach((navA) => {
+        navA.addEventListener("click", () => {
+            if (spans[0].className === "actived") {
+                setTimeout(() => {
+                    spans.forEach((span) => {
+                        span.classList.remove("actived");
+                    })
+                    navBar.style.top = -120 + "%";
+                }, 100);
+            }
+        })
+    })
+})();
+
+/* 滑動到各section 加上slowMoveRight動畫的效果 */
+(function () {
+    let home = document.querySelector(".home");
+    let about = document.querySelector(".about");
+    let portfolio = document.querySelector(".portfolio");
+    let experienceBoxes = document.querySelectorAll(".experience .box");
+    let distance = home.offsetHeight + about.offsetHeight + portfolio.offsetHeight - 300;
+    document.addEventListener("scroll", () => {
+        let scrollBar = window.scrollY;
+        if (scrollBar >= home.offsetHeight / 2) {
+            about.classList.add("slowMoveRight");
+        }
+
+        if (scrollBar >= distance) {
+            experienceBoxes.forEach((box) => {
+                box.classList.add("slowMoveRight");
+            })
+        }
+    })
+})();
+
+/* 實現hobby圖片懶加載 */
+
+(function () {
+    const images = document.querySelectorAll('img[data-src]');
+
+    // 設定在什麼情況下觸發 callback 函式
+    const options = {
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 0
+    }
+
+    const lazyLoad = (entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            const src = entry.target.getAttribute("data-src");
+            if (!src) return;
+            entry.target.src = src;
+            observer.unobserve(entry.target);
+        })
+    };
+
+    let observer = new IntersectionObserver(lazyLoad, options);
+    images.forEach(image => observer.observe(image));
+})();
+
+
+/* Hobby區的選擇性顯示圖片的效果 */
+(function () {
+    let hobbyBtns = document.querySelectorAll(".hobby .controls li");
+    let hobbyImages = document.querySelectorAll(".hobby .image-list li");
+
+    hobbyBtns.forEach((hobbyBtn, index) => {
+        hobbyBtn.addEventListener("click", () => {
+            // 按鈕部分
+            hobbyBtns.forEach((hobbyBtn) => {
+                hobbyBtn.classList.remove("active");
+            })
+            hobbyBtn.classList.toggle("active");
+
+            // 圖片部分
+            let classList = ["hobby-image", "trekker", "ecology", "cuisine"];
+            hobbyImages.forEach((hobbyImage) => {
+                hobbyImage.classList.remove("d-none");
+                if (!hobbyImage.classList.contains(`${classList[index]}`)) {
+                    hobbyImage.classList.add("d-none");
+                }
+            })
+        })
+    })
+})();
+
+
+
+
